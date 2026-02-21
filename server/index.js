fe6db0715f2f944d1690a5e71dbef787b6273219
devang-p12 <patildevang843@gmail.com>
2026-02-21 15:20:56 +0530

    custom exercise create

--- server/index.js
+++ server/index.js
@@ -17,7 +17,8 @@ const app = express();
 const server = createServer(app);
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 // Connect to database
 await connectDB();
